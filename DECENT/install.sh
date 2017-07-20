521c4ed9924344f234cd21791e92ef0e39c14796
Zander Olidan <kaikaiolidan@gmail.com>
2017-07-20 23:52:03 +0800

    Finished gen_genesis

--- DECENT/install.sh
+++ DECENT/install.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-echo "Installing prerequisites"
+echo "Installing prerequisites..."
+chmod +x prereq.sh
 prereq.sh
 
-echo "Installing Boost"
+echo "Installing Boost..."
 BOOST_ROOT="~/opt/boost_1_60_0"
 
 if [ ! -d "$BOOST_ROOT" ]; then
+    chmod +x boost_install.sh 
     boost_install.sh
 fi
 
 echo "Generating keys..."
+chmod +x gen_config.sh
 gen_config.sh
 
+echo "Generating genesis..."
+chmod +x gen_genesis.sh
+gen_genesis.sh
 
 
