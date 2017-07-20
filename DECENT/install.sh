583335477a8368d10d998bd6323593a79901c2ea
Zander Olidan <kaikaiolidan@gmail.com>
2017-07-20 23:18:12 +0800

    finished preliminary shell installs and organization

--- DECENT/install.sh
+++ DECENT/install.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
 echo "Installing prerequisites"
+prereq.sh
+
+echo "Installing Boost"
+BOOST_ROOT="~/opt/boost_1_60_0"
+
+if [ ! -d "$BOOST_ROOT" ]; then
+    boost_install.sh
+fi
+
+echo "Generating keys..."
+gen_config.sh
+
+
 
