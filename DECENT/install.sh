f9d2d8714a715296db840a4453a6b008875e3bbc
Zander Olidan <kaikaiolidan@gmail.com>
2017-07-21 06:26:14 +0800

    Finished installation shell script

--- DECENT/install.sh
+++ DECENT/install.sh
@@ -4,33 +4,40 @@
 # This install.sh is merely the main function to execute them
 # Comment out unwanted scripts if reinstalling (i.e Boost)
 
+remake () {
+    cd DECENT-Network-build
+    cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DGRAPHENE_EGENESIS_JSON="$origDir/generated/genesis.json" $origDir/DECENT-Network
+    cmake --build . --target all -- -j -l 3.0
+    cmake --build . --target install
+}
+
 scriptsDir="scripts"
 
 # Prerequisites
-echo "Installing prerequisites..."
-chmod +x $scriptsDir/prereq.sh
-./$scriptsDir/prereq.sh
+#echo "Installing prerequisites..."
+#chmod +x $scriptsDir/prereq.sh
+#./$scriptsDir/prereq.sh
 
 # Boost Library
-echo "Installing Boost..."
-BOOST_ROOT="~/opt/boost_1_60_0"
+#echo "Installing Boost..."
+#BOOST_ROOT="~/opt/boost_1_60_0"
 
-if [ ! -d "$BOOST_ROOT" ]; then
-    chmod +x $scriptsDir/boost_install.sh 
-    ./$scriptsDir/boost_install.sh
-fi
+#if [ ! -d "$BOOST_ROOT" ]; then
+    #chmod +x $scriptsDir/boost_install.sh 
+    #./$scriptsDir/boost_install.sh
+#fi
 
-# For generatking keys and config
-echo "Generating config file..."
-chmod +x $scriptsDir/gen_config.sh
-./$scriptsDir/gen_config.sh
+## For generatking keys and config
+#echo "Generating config file..."
+#chmod +x $scriptsDir/gen_config.sh
+#./$scriptsDir/gen_config.sh
 
-# For generating the genesis file. 
-echo "Generating genesis..."
-chmod +x $scriptsDir/gen_genesis.sh
-./$scriptsDir/gen_genesis.sh
+## For generating the genesis file. 
+#echo "Generating genesis..."
+#chmod +x $scriptsDir/gen_genesis.sh
+#./$scriptsDir/gen_genesis.sh
 
+chmod +x $scriptsDir/decent_install.sh
+./$scriptsDir/decent_install.sh
 
-#1. Get decent 
-#2. Make with genesis embedded https://github.com/DECENTfoundation/DECENT-Network/wiki/private-testnet#embedding-genesis-optional
-#3. Create files with datafile and config copied
+$@
